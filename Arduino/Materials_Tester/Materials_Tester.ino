4af8d35626cb654c8eca648b48cae10ca10d39b1
Sbudez <152808138+Sbudez@users.noreply.github.com>
2024-03-20 09:59:32 +0100

    Update Materials_Tester.ino

--- Arduino/Materials_Tester/Materials_Tester.ino
+++ Arduino/Materials_Tester/Materials_Tester.ino
@@ -1,28 +1,28 @@
-/* VernierLibTutorialAnalogRead (v2017)
- * This sketch reads a data point from a Vernier Analog (BTA) 
- * sensor once every half second and prints the sensor reading 
- * with units to the Serial Monitor.
+/* VernierTutorialAnalogVoltage (v2017)
+ * This sketch reads the raw count from a Vernier Analog (BTA) 
+ * sensor once every half second, converts it to a voltage, 
+ * and prints both values to the Serial Monitor.
  * 
- * Plug the sensor into the Analog 1 port on the Vernier Arduino 
+ * Plug the sensor into the Analog 2 port on the Vernier Arduino 
  * Interface Shield or into an Analog Protoboard Adapter wired 
- * to Arduino pin A0.
+ * to Arduino pin A2.
  */
  
-#include "VernierLib.h" //include Vernier functions in this sketch
-VernierLib Vernier; //create an instance of the VernierLib library
- 
+float rawCount; //create global variable for reading from A/D converter (0-1023)
+float voltage; //create global variable for voltage (0-5V)
  
 void setup() {
   Serial.begin(9600);
   while (!Serial);
   delay(4000);    //Need time for the Serial Monitor to become available
-  Vernier.autoID(); //identify the sensor being used
 }
  
 void loop() {
-  float sensorReading = Vernier.readSensor(); //read one data value
-  Serial.print(sensorReading); //print data value 
-  Serial.print(" "); //print a space
-  Serial.println(Vernier.sensorUnits()); //print units and skip to next line
-  delay(1000); //wait half second
+  rawCount = analogRead(A0); //read one data value (0-1023)
+  voltage = rawCount/1023*5; //convert raw count to voltage (0-5V)
+  Serial.print(rawCount); //print raw count 
+  Serial.print("\t"); //print a tab character
+  Serial.println(voltage); //print voltage and skip to next line
+  delay(500); //wait half second
 }
+ 
