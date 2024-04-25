e3df5759397a8d03f83e60de3235e774de7dded2
Sbudez <152808138+Sbudez@users.noreply.github.com>
2024-04-25 16:08:14 +0200

    Update Materials_Tester.ino

--- Arduino/Materials_Tester/Materials_Tester.ino
+++ Arduino/Materials_Tester/Materials_Tester.ino
@@ -1,13 +1,3 @@
-/* VernierTutorialAnalogVoltage (v2017)
- * This sketch reads the raw count from a Vernier Analog (BTA) 
- * sensor once every half second, converts it to a voltage, 
- * and prints both values to the Serial Monitor.
- * 
- * Plug the sensor into the Analog 2 port on the Vernier Arduino 
- * Interface Shield or into an Analog Protoboard Adapter wired 
- * to Arduino pin A2.
- */
- 
 float rawCount; //create global variable for reading from A/D converter (0-1023)
 float voltage; //create global variable for voltage (0-5V)
  
@@ -25,4 +15,3 @@ void loop() {
   Serial.println(voltage); //print voltage and skip to next line
   delay(500); //wait half second
 }
- 
